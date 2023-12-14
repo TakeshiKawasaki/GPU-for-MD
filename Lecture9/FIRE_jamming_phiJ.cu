329f960d4208665c704b69a9d7d0df8da2708be8
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2023-12-14 22:39:51 +0900

    Update FIRE_jamming_phiJ.cu

--- Lecture9/FIRE_jamming_phiJ.cu
+++ Lecture9/FIRE_jamming_phiJ.cu
@@ -285,8 +285,8 @@ __global__ void volume_affine(double *x_dev, double *y_dev,double *phi_dev,doubl
   __syncthreads();
   
   if(i_global==0){
-    *phi_dev += *deltaphi_dev;
     *L_dev   *= sqrt(*phi_dev/(*phi_dev+*deltaphi_dev));
+    *phi_dev += *deltaphi_dev;
   }
 }
 
